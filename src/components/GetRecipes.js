2b4396b74f8e84e6182ce6626cc79ba53a6dcff1
waqarakram93-tech <84813788+waqarakram93-tech@users.noreply.github.com>
2021-07-29 17:38:08 +0200

    geeting api data

--- src/components/GetRecipes.js
+++ src/components/GetRecipes.js
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from 'react'
-import { client } from '../client'
+//import { client } from '../client'
 import RecipeCart from './RecipeCart'
 
 //import React from 'react'
 
 function GetRecipes() {
 
+    const [loading, setLoading] = useState(true)
     const [recipes, setRecipes] = useState([])
 
     useEffect(() => {
-        client.getEntries()
-            .then((response) => {
-
-                setRecipes(response.items)
-
+        setLoading(true)
+        fetch("http://localhost:8000/recipes")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setRecipes(data.recipes)
+                setLoading(false)
             })
 
 
     }, [])
+    if (loading) return <>Loading</>
     return (
         <>
             {
--- src/components/RecipeCart.js
+++ src/components/RecipeCart.js
@@ -6,11 +6,11 @@ const RecipeCart = ({ recipe }) => {
     return (
         <div className="first row d-flex justify-content-center">
             <div className="title  col-md-4 mt-3">
-                <h1 >{recipe.fields.title}</h1>
+                <h1 >{recipe.title}</h1>
             </div>
             <div className="second row">
                 <p className="description col-md-8 mx-auto pt-2 mt-4">
-                    {recipe.fields.description}
+                    {recipe.description}
                 </p>
             </div>
             <div className="third row">
@@ -19,13 +19,13 @@ const RecipeCart = ({ recipe }) => {
                         Ingredients
                     </div>
                     <ul>
-                        {recipe.fields.ingred.map(ing => <li>{ing}</li>)}
+                        {/* {recipe.ingred.map(ing => <li>{ing}</li>)} */}
                     </ul>
                 </div>
                 <div className=' col-md-8 m col-sm-4 col-xs-12'>
                     <div className="row justify-content-center">
                         <div className='col-md-4'>
-                            <img className="image " src={recipe.fields.images.fields.file.url + '?w=450&h=450'} alt={recipe.fields.title} />
+                            {/* <img className="image " src={recipe.fields.images.fields.file.url + '?w=450&h=450'} alt={recipe.fields.title} /> */}
                         </div>
                     </div>
                 </div>
